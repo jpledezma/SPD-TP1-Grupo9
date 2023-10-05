cabf489dbd236986d0ddcdc381d15ba32b977201
Juan <jpledezma01@gmail.com>
2023-10-05 01:15:39 -0300

    Se elimina el directorio duplicado

--- SPD-TP1-Grupo9/README.md
+++
@@ -1,1 +0,0 @@
-# SPD-TP1-Grupo9
--- main.ino
+++ main.ino
@@ -1,1 +1,15 @@
-// Intentá modificar este archivo y hacer un commit para verificar que funcione
+// C++ code
+// Juan Pablo Ledezma e Iván Laurito - Grupo 9
+
+void setup()
+{
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void loop()
+{
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(1000); // Wait for 1000 millisecond(s)
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(1000); // Wait for 1000 millisecond(s)
+}
