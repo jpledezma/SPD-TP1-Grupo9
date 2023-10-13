f3b3245aef3fe065e2d7504e804e0fc39c5981fe
Juan <jpledezma01@gmail.com>
2023-10-13 19:14:08 -0300

    Se agregan funciones: detectar pulsación y normalizar contador

--- funciones.ino
+++ funciones.ino
@@ -105,3 +105,51 @@ void apagarDisplay(){
   digitalWrite(F, LOW);
   digitalWrite(G, LOW);
 }
+
+// Estructura con 2 tipos de datos: el estado anterior de la entrada digital, y la ejecución de la acción que efectúa esa entrada
+typedef struct {
+  bool ejecucion;
+  bool estado_anterior;
+}estructura;
+
+
+// función para ejecutar una acción por única vez en base a una entrada digital (por ejemplo, un pulsador)
+estructura detectarPulsacion(bool estado_actual, bool estado_anterior){
+  estructura devolucion;
+  devolucion.estado_anterior = estado_anterior;
+  
+  if (estado_actual != estado_anterior){
+    devolucion.estado_anterior = estado_actual;
+    devolucion.ejecucion = estado_actual;
+  }
+  else{
+    devolucion.ejecucion = false;
+  }
+      
+  return devolucion;
+}
+
+// función para evitar que el contador tome valores fuera de los límites establecidos (entre 0 y 99)
+int normalizarContador(int contador){
+
+  if (contador > 99){
+    contador = contador - 100;
+  }
+  if (contador < 0){
+    contador = 100 + contador;
+  }
+
+  return contador;
+}
+// Idem al anterior, pero permite seleccionar el límite superior (el límite inferior siempre es 0)
+int normalizarContador2(int contador, int limite_superior){
+
+  if (contador > limite_superior ){
+    contador = contador - limite_superior - 1;
+  }
+  if (contador < 0){
+    contador = limite_superior + 1 + contador;
+  }
+
+  return contador;
+}
